486225e4aee0e6a12d452c815c2b2a41b0749b0a
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-12 08:43:24 +0300

    init: astro template

--- src/app/(root)/projects/layout.tsx
+++
@@ -1,9 +0,0 @@
-type ProjectsLayoutProps = {
-  children: React.ReactNode;
-};
-
-const ProjectsLayout = ({ children }: ProjectsLayoutProps) => {
-  return <div className="overflow-hidden pb-6">{children}</div>;
-};
-
-export default ProjectsLayout;
