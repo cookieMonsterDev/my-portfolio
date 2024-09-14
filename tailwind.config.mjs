786d75ebcc7c9eeeb6db58ca0318305ac6ca2eb4
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-14 19:15:50 +0300

    feat: add some more icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,8 +9,19 @@ export default {
         center: true,
       },
       colors: {
-        pimary: "#6b705c",
-        secondary: "#dbd0bd",
+        "brown-1": "hsl(36 39% 88%)",
+        "brown-2": "hsl(35 43% 53%)",
+        "brown-3": "hsl(31 41% 48%)",
+        "brown-4": "hsl(28 40% 40%)",
+        "brown-5": "hsl(26 36% 34%)",
+        "brown-6": "hsl(25 34% 28%)",
+      },
+      textColor: {
+        foreground: "hsl(36 45% 20%)",
+        "muted-foreground": "hsl(36 45% 25%)",
+      },
+      borderColor: {
+        DEFAULT: "hsl(36 45% 60%)",
       },
     },
   },
